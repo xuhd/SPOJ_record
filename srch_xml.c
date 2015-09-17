513a01b76f886b17726a41b6de06e14afbb48b95
xuhaodong <ntu_rivaldo@163.com>
2015-09-17 16:01:00 -0700

    pass in my test cases, but failed in SPOJ

--- srch_xml.c
+++ srch_xml.c
@@ -19,8 +19,9 @@ typedef struct _node_t{
   int len; //tag name len
   int num; 
   int level;
-  struct _node_t* l;
-  struct _node_t* r;
+  struct _node_t* p; //parent
+  struct _node_t* l; //first child
+  struct _node_t* r; //next sibling
 } node_t, *tree_t;
 
 static char sg_buf[100*1024]; //input file max 100KB
@@ -59,34 +60,65 @@ tag_type_t get_next_tag(char** pp, int* plen)
 }
 
 #define MAX_MATCH_CNT (8192)
-#define MAX_MATCH_LEN (8192)
-static int sg_match_path[MAX_MATCH_CNT][MAX_MATCH_LEN];
+static int sg_match[MAX_MATCH_CNT];
 
 void match_tree(tree_t tree, tree_t ptn)
 {
-  static int m;
   if(NULL==tree || NULL==ptn)return;
+  //printf("%s\n", tree->p_name);
   if(0 == strcmp(tree->p_name, ptn->p_name)) {
-    sg_match_path[sg_match_cnt][m++] = tree->num;
-    if( NULL == ptn->l && NULL == ptn->r ){
-      m = 0;
-      sg_match_cnt++;
-      //printf("%d\n", tree->num);
-    }else if(tree->l && ptn->l)
-      match_tree(tree->l, ptn->l);
-    else if(tree->r && ptn->r)
-      match_tree(tree->r, ptn->r);
+    if( 0 == visit(ptn, tree) ){
+      sg_match[sg_match_cnt++] = tree->num;
+    }
   }
 
   if(tree->l)match_tree(tree->l, ptn);
   if(tree->r)match_tree(tree->r, ptn);
 }
 
-node_t* make_tree_node(char* pname, int len, int level)
+
+int visit(tree_t ptn_tree, tree_t tree)
+{
+  int top = -1;
+  node_t* node_stack[2][MAX_DEPTH];
+  node_t *p, *pr;
+
+  if(NULL == ptn_tree) return -1;
+  if(NULL == tree) return -1;
+
+  node_stack[0][++top] = ptn_tree;
+  node_stack[1][top] = tree;
+  while( top >= 0 ) {
+    p = node_stack[0][top];
+    pr = node_stack[1][top];
+    pr = pr->p?pr->p->l:pr;
+    while(pr && strcmp(p->p_name, pr->p_name))
+      pr = pr->r;
+    if(NULL == pr) return -1;
+    //pr = node_stack[1][top--]; //reset pr
+    top--;
+
+    if(p->r) {
+      node_stack[0][++top] = p->r;
+      node_stack[1][top] = pr;
+    }
+    if(p->l) {
+      node_stack[0][++top] = p->l;
+      if(pr->l)
+	node_stack[1][top] = pr->l;
+      else return -1;
+    }
+  }
+
+  return 0;
+}
+
+node_t* make_tree_node(node_t* p_parent, char* pname, int len, int level)
 {
   static int node_idx;
   node_t* p_node = malloc(sizeof(node_t));
 
+  p_node->p = p_parent;
   p_node->p_name = pname;
   p_node->len = len;
   p_node->level = level;
@@ -115,6 +147,7 @@ int main()
   int top;
   node_t* node_stack[MAX_DEPTH];
   node_t* p;
+  node_t* p_parent;
   tree_t sub_tree[MAX_DEPTH];
   tag_type_t t;
 
@@ -130,12 +163,13 @@ int main()
     }
 
     get_next_tag(&pname, &len);
-    node_stack[++top] = make_tree_node(pname, len, top);
+    node_stack[++top] = make_tree_node(NULL, pname, len, top);
     while(top >= 0) {
       t = get_next_tag(&pname, &len);
-      if(TAG_START == t)
-	node_stack[++top] = make_tree_node(pname, len, top);
-      else if(TAG_END == t) {
+      if(TAG_START == t) {
+	p_parent = node_stack[top];
+	node_stack[++top] = make_tree_node(p_parent, pname, len, top);
+      } else if(TAG_END == t) {
 	node_t** pp;
 	node_t *pn, *tmp;
 
@@ -164,9 +198,10 @@ int main()
 
   printf("%d\n", sg_match_cnt);
   for(i = 0; i < sg_match_cnt; i++)
-    printf("%d\n", sg_match_path[i][0]);
+    printf("%d\n", sg_match[i]);
 
   free_tree(tree);
   free_tree(ptn_tree);
   return 0;
 }
+
